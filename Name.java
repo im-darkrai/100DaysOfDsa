01a669b68f7a8f5336089091d8b7112551e09aa2
im-darkrai <mjha1422@gmail.com>
2025-03-19 02:26:25 +0530

    Concatination , string Length ,Char At

--- Name.java
+++ Name.java
@@ -6,5 +6,7 @@ public class Name {
         
         System.out.println(C); // Output: MukundBanty
         System.out.println(C.length()); // Output: 12
+        System.out.println("" + C.charAt(0)+C.charAt(6)); // Output: MB
+
     }
 }
