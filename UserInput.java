8cd3b64170d9feb5f6abb01ed089427719a0114f
im-darkrai <mjha1422@gmail.com>
2025-03-19 11:02:55 +0530

    Learn how to take Input from user in a Java

---
+++ UserInput.java
@@ -0,0 +1,13 @@
+import java. util. Scanner;
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String Name = sc.nextLine();
+        int Age = sc.nextInt();
+        System.out.println("Name = " + Name + ", Age = " + Age);
+
+    }
+  
+
+    }
+
