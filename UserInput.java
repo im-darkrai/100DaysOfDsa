0cb5c4c5a72be81f95b97d124172a28077f5fe3e
im-darkrai <mjha1422@gmail.com>
2025-03-20 11:45:56 +0530

    How to Take User Input

--- UserInput.java
+++ UserInput.java
@@ -1,13 +1,14 @@
-import java. util. Scanner;
+// Source code is decompiled from a .class file using FernFlower decompiler.
+import java.util.Scanner;
+
 public class UserInput {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        String Name = sc.nextLine();
-        int Age = sc.nextInt();
-        System.out.println("Name = " + Name + ", Age = " + Age);
+   public UserInput() {
+   }
 
-    }
-  
-
-    }
-
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      String var2 = var1.nextLine();
+      int var3 = var1.nextInt();
+      System.out.println("Name = " + var2 + ", Age = " + var3);
+   }
+}
